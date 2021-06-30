861b76071f1e96628f9b7769508d90f2004c6be4
Candan CEBIROGLU <ccebiroglu74@gmail.com>
2021-07-01 01:50:25 +0300

    commit4

--- odevler/01-odev1/js/odev1.js
+++ odevler/01-odev1/js/odev1.js
@@ -2,3 +2,4 @@ let userName = prompt("Adınızı giriniz")
 let info = document.querySelector("#info")
 
 info.innerHTML = `Merhaba ${userName}! Hoşgeldin!`
+
--- odevler/01-odev1/odev1.js
+++
@@ -1,4 +0,0 @@
-let userName = prompt("Adınızı giriniz")
-let info = document.querySelector("#info")
-
-info.innerHTML = `Merhaba ${userName}! Hoşgeldin!`

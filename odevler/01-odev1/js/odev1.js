a82db577e1d3ee5e02caec3120bafb6e8753434f
Candan CEBIROGLU <ccebiroglu74@gmail.com>
2021-07-01 17:01:04 +0300

    timeOK commit

--- odevler/01-odev1/js/odev1.js
+++ odevler/01-odev1/js/odev1.js
@@ -1,5 +1,84 @@
-let userName = prompt("Adınızı giriniz")
+/*let userName = prompt("Adınızı giriniz")
 let info = document.querySelector("#info")
 
-info.innerHTML = `Merhaba ${userName}! Hoşgeldin!`
+info.innerHTML = `Merhaba ${userName}! Hoşgeldin!`*/
 
+let tarih = new Date();
+let saat= tarih.getHours();
+let dakika = tarih.getMinutes();
+let saniye = tarih.getSeconds();
+var i = 0;
+function kronometre(){
+    let time = document.querySelector("#time")
+    
+
+    let strSaat;
+    let strSaniye;
+    let strDakika;
+    strSaniye = saniye.toString();
+    strDakika = dakika.toString();
+    strSaat = saat.toString();
+
+    let temp; let zero = '0'; let temp2;
+
+    if(saniye < 9){
+        saniye++;
+        temp = saniye.toString();
+        strSaniye = zero.concat(temp);
+    }else if(saniye >58){
+        dakika++;
+        strDakika = dakika.toString();
+
+        saniye = 0;
+        temp = saniye.toString();
+        strSaniye = zero.concat(temp);
+    }
+    else{
+        saniye++;
+        strSaniye = saniye.toString();
+        strDakika = dakika.toString();
+    }
+
+    if(dakika > 59){
+        saat++;
+        strSaat = saat.toString();
+
+        dakika = 0;
+        temp2 = dakika.toString();
+        strDakika = zero.concat(temp2);
+
+        saniye = 0;
+        temp = saniye.toString();
+        strSaniye = zero.concat(temp);
+
+    }
+
+    time.innerHTML = `${strSaat}:${strDakika}:${strSaniye} `
+
+
+
+
+
+    /*tarih.getSeconds()+i;
+    i++;
+    let temp ;
+    let saniye ;
+    let zero = "0";
+    if(saniye < 10){
+        temp = tarih.getSeconds().toString();
+        saniye = zero.concat(temp);
+    }else if(saniye > 60){
+        tarih.getMinutes() +1;
+        saniye = 0;
+    }*/
+
+
+
+   
+
+    /*console.log(tarih.getHours())
+    console.log(tarih.getMinutes())
+    console.log(tarih.getSeconds())
+    console.log(tarih.getSeconds()+1)*/
+}
+setInterval("kronometre()",1000)
